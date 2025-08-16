94ba06294623f843d3f93eeaee1aa1e494eecd69
IsacQ <isacqueirozmarques2006@gmail.com>
2025-08-16 19:00:05 -0300

    modificação no css deixando melhor estilizado, js coloqueipara o input puxar a data atual e ter uma data minima para nao pegar data do passado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+import dayjs from "dayjs";
 import "./style.css";
 console.log("Hello, World!");
+var inputdata = document.getElementById("data");
+var datahj = dayjs().format("YYYY-MM-DD");
+inputdata.value = datahj;
+inputdata.min = datahj;
